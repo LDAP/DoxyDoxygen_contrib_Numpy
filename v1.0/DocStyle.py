a35232d51b3e59b5a5b091421689ee00ca10cddf
Lucas Alber <lucasd.alber@gmail.com>
2021-01-16 16:57:23 +0100

    Implemented uncommented_text_to_lines

--- v1.0/DocStyle.py
+++ v1.0/DocStyle.py
@@ -3,17 +3,22 @@ author: Lucas Alber <lucasd.alber@gmail.com>
 """
 
 import os
+import re
 
 __license__ = "https://raw.githubusercontent.com/20Tauri/DoxyDoxygen_contrib_HeaderDoc/master/LICENSE.md"
+
+param_header = 'Parameters' + os.linesep + '----------'
+return_header = 'Returns' + os.linesep + '-------'
+raises_header = 'Raises' + os.linesep + '------'
 
 COMMANDS_LIST = [
     DoxyCommand('@_hidden_param',  '<parameter_name> : { parameter_type }' + os.linesep + '    { parameter_description }', key_index=0,   help="Documents the parameter to a function"),
     DoxyCommand('@_hidden_return', '{ return_value_type }' + os.linesep + '    { description_of_the_return_value }', help="Documents the return value of a function"),
     DoxyCommand('@_hidden_raises', '{ exception_object }' + os.linesep + '    { exception_description }', key_index=0, help="Starts an exception description for an exception object with name <exception-object>"),
 
-    DoxyCommand('@_hidden_param_header', '<workaround>Parameters' + os.linesep + '----------'),
-    DoxyCommand('@_hidden_return_header', '<workaround>Returns' + os.linesep + '-------'),
-    DoxyCommand('@_hidden_raises_header', '<workaround>Raises' + os.linesep + '------'),
+    DoxyCommand('@_hidden_param_header', '<workaround>' + param_header),
+    DoxyCommand('@_hidden_return_header', '<workaround>' + return_header),
+    DoxyCommand('@_hidden_raises_header', '<workaround>' + raises_header),
 ]
 
 
@@ -66,6 +71,26 @@ class DocStyle(DocStyleBase):
             "@_hidden_raises_header"
         ]
 
+    def fun(a: int, b) -> str:
+        """
+        { function_description }
+
+        Parameters
+        ----------
+        a : int
+            { parameter_description }
+        b : { parameter_type }
+            { parameter_description }
+
+        Returns
+        -------
+        str
+            { description_of_the_return_value }
+        """
+        if b:
+            raise IndexError()
+        return ""
+
     def uncommented_text_to_lines(self, uncommented_text):
         """
         Implements a custom DocBlock parser for this DocStyle (all plugins that use
@@ -108,4 +133,34 @@ class DocStyle(DocStyleBase):
             ('@_hidden_Parameter', ['y', None, 'Description of parameter `y` (with type not specified).']),
         ]
         """
-        pass
+        parse_result = []
+
+        brief_regex = re.compile("^.+$", re.MULTILINE)
+        match = brief_regex.search(uncommented_text)
+        if match:
+            parse_result += ('@_brief', [match.group()])
+
+        param_header_regex = re.compile(param_header.replace(os.linesep, '\n'))
+        if param_header_regex.search(uncommented_text):
+            parse_result += ('@_hidden_param_header', [""])
+        return_header_regex = re.compile(return_header.replace(os.linesep, '\n'))
+        if return_header_regex.search(uncommented_text):
+            parse_result += ('@_hidden_return_header', [""])
+        raises_header_regex = re.compile(raises_header.replace(os.linesep, '\n'))
+        if raises_header_regex.search(uncommented_text):
+            parse_result += ('@_hidden_raises_header', [""])
+
+        param_regex = re.compile("(?P<param_name>.*?)\\s:\\s(?P<param_type>.*?)\n\\s{4}(?P<param_desc>.*)")
+        for match in param_regex.finditer(uncommented_text):
+            parse_result += ('@_hidden_param', [match.group('param_name'), match.group('param_type'), match.group('param_desc')])
+
+        return_regex = re.compile("^(?P<return_type>[^\n:]*?)\n\\s{4}(?P<return_desc>.*)$", re.MULTILINE)
+        match = return_regex.search(uncommented_text)
+        if (match):
+            parse_result += ('@_hidden_return', [match.group('return_type'), match.group('return_desc')])
+
+        raises_regex = re.compile("Raises.*^(?P<raise_type>[^\n:]*?)\n\\s{4}(?P<raise_desc>.*?)$", re.DOTALL | re.MULTILINE)
+        for match in raises_regex.finditer(uncommented_text):
+            parse_result += ('@_hidden_raises', [match.group('raise_type'), match.group('raise_desc')])
+
+        return parse_result
