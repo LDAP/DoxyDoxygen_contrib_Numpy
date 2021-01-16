18f42cb6a657de827d871c63418fc8b30c48c45d
Lucas Alber <lucasd.alber@gmail.com>
2021-01-16 18:03:14 +0100

    Format code with black

--- v1.0/DocStyle.py
+++ v1.0/DocStyle.py
@@ -5,34 +5,52 @@ author: Lucas Alber <lucasd.alber@gmail.com>
 import os
 import re
 
-__license__ = "https://raw.githubusercontent.com/LDAP/DoxyDoxygen_contrib_Numpy/master/LICENSE.md"
+__license__ = (
+    "https://raw.githubusercontent.com/LDAP/DoxyDoxygen_contrib_Numpy/master/LICENSE.md"
+)
 
-param_header = 'Parameters' + os.linesep + '----------'
-return_header = 'Returns' + os.linesep + '-------'
-raises_header = 'Raises' + os.linesep + '------'
+param_header = "Parameters" + os.linesep + "----------"
+return_header = "Returns" + os.linesep + "-------"
+raises_header = "Raises" + os.linesep + "------"
 
 COMMANDS_LIST = [
-    DoxyCommand('@_hidden_param',  '<parameter_name> : { parameter_type }' + os.linesep + '    { parameter_description }', key_index=0,   help="Documents the parameter to a function"),
-    DoxyCommand('@_hidden_return', '{ return_value_type }' + os.linesep + '    { description_of_the_return_value }', help="Documents the return value of a function"),
-    DoxyCommand('@_hidden_raises', '{ exception_object }' + os.linesep + '    { exception_description }', key_index=0, help="Starts an exception description for an exception object with name <exception-object>"),
-
-    DoxyCommand('@_hidden_param_header', '<workaround>' + param_header),
-    DoxyCommand('@_hidden_return_header', '<workaround>' + return_header),
-    DoxyCommand('@_hidden_raises_header', '<workaround>' + raises_header),
+    DoxyCommand(
+        "@_hidden_param",
+        "<parameter_name> : { parameter_type }"
+        + os.linesep
+        + "    { parameter_description }",
+        key_index=0,
+        help="Documents the parameter to a function",
+    ),
+    DoxyCommand(
+        "@_hidden_return",
+        "{ return_value_type }"
+        + os.linesep
+        + "    { description_of_the_return_value }",
+        help="Documents the return value of a function",
+    ),
+    DoxyCommand(
+        "@_hidden_raises",
+        "{ exception_object }" + os.linesep + "    { exception_description }",
+        key_index=0,
+        help="Starts an exception description for an exception object with name <exception-object>",
+    ),
+    DoxyCommand("@_hidden_param_header", "<workaround>" + param_header),
+    DoxyCommand("@_hidden_return_header", "<workaround>" + return_header),
+    DoxyCommand("@_hidden_raises_header", "<workaround>" + raises_header),
 ]
 
 
 class DocStyle(DocStyleBase):
-    ## Name of this style (will be used in your `profiles` settings)
     name = "Numpy"
 
     typename_to_decorated = {
-        EvaluatorBase.TYPE_STRING:  "str",
+        EvaluatorBase.TYPE_STRING: "str",
         EvaluatorBase.TYPE_INTEGER: "int",
         EvaluatorBase.TYPE_BOOLEAN: "bool",
-        EvaluatorBase.TYPE_FLOAT:   "float",
-        EvaluatorBase.TYPE_ARRAY:   "Array",
-        EvaluatorBase.TYPE_THIS:    "Object",
+        EvaluatorBase.TYPE_FLOAT: "float",
+        EvaluatorBase.TYPE_ARRAY: "Array",
+        EvaluatorBase.TYPE_THIS: "Object",
     }
 
     def __init__(self):
@@ -45,19 +63,27 @@ class DocStyle(DocStyleBase):
         commands_list = []
 
         if definition["params"]:
-            commands_list += self.command('@_hidden_param_header', [""])
+            commands_list += self.command("@_hidden_param_header", [""])
             for param in definition["params"]:
-                commands_list += self.command("@_hidden_param", [param.name, decorated_type(param.typename)])
+                commands_list += self.command(
+                    "@_hidden_param", [param.name, decorated_type(param.typename)]
+                )
 
         definition_return = definition["return"]
-        if definition_return and definition_return != "void" and definition_return != "None":
+        if (
+            definition_return
+            and definition_return != "void"
+            and definition_return != "None"
+        ):
             commands_list += self.command("@_hidden_return_header", [""])
             commands_list += self.command("@_hidden_return", [definition_return])
 
         if definition["throws"]:
-            commands_list += self.command('@_hidden_raises_header', [""])
+            commands_list += self.command("@_hidden_raises_header", [""])
             for type_name in definition["throws"]:
-                commands_list += self.command("@_hidden_raises", [decorated_type(type_name)])
+                commands_list += self.command(
+                    "@_hidden_raises", [decorated_type(type_name)]
+                )
 
         return commands_list
 
@@ -68,7 +94,7 @@ class DocStyle(DocStyleBase):
             "@_hidden_raises",
             "@_hidden_param_header",
             "@_hidden_return_header",
-            "@_hidden_raises_header"
+            "@_hidden_raises_header",
         ]
 
     def uncommented_text_to_lines(self, uncommented_text):
@@ -118,29 +144,49 @@ class DocStyle(DocStyleBase):
         brief_regex = re.compile("^.+$", re.MULTILINE)
         match = brief_regex.search(uncommented_text)
         if match:
-            parse_result += ('@_brief', [match.group()])
+            parse_result += ("@_brief", [match.group()])
 
-        param_header_regex = re.compile(param_header.replace(os.linesep, '\n'))
+        param_header_regex = re.compile(param_header.replace(os.linesep, "\n"))
         if param_header_regex.search(uncommented_text):
-            parse_result += ('@_hidden_param_header', [""])
-        return_header_regex = re.compile(return_header.replace(os.linesep, '\n'))
+            parse_result += ("@_hidden_param_header", [""])
+        return_header_regex = re.compile(return_header.replace(os.linesep, "\n"))
         if return_header_regex.search(uncommented_text):
-            parse_result += ('@_hidden_return_header', [""])
-        raises_header_regex = re.compile(raises_header.replace(os.linesep, '\n'))
+            parse_result += ("@_hidden_return_header", [""])
+        raises_header_regex = re.compile(raises_header.replace(os.linesep, "\n"))
         if raises_header_regex.search(uncommented_text):
-            parse_result += ('@_hidden_raises_header', [""])
+            parse_result += ("@_hidden_raises_header", [""])
 
-        param_regex = re.compile("(?P<param_name>.*?)\\s:\\s(?P<param_type>.*?)\n\\s{4}(?P<param_desc>.*)")
+        param_regex = re.compile(
+            "(?P<param_name>.*?)\\s:\\s(?P<param_type>.*?)\n\\s{4}(?P<param_desc>.*)"
+        )
         for match in param_regex.finditer(uncommented_text):
-            parse_result += ('@_hidden_param', [match.group('param_name'), match.group('param_type'), match.group('param_desc')])
+            parse_result += (
+                "@_hidden_param",
+                [
+                    match.group("param_name"),
+                    match.group("param_type"),
+                    match.group("param_desc"),
+                ],
+            )
 
-        return_regex = re.compile("^(?P<return_type>[^\n:]*?)\n\\s{4}(?P<return_desc>.*)$", re.MULTILINE)
+        return_regex = re.compile(
+            "^(?P<return_type>[^\n:]*?)\n\\s{4}(?P<return_desc>.*)$", re.MULTILINE
+        )
         match = return_regex.search(uncommented_text)
-        if (match):
-            parse_result += ('@_hidden_return', [match.group('return_type'), match.group('return_desc')])
+        if match:
+            parse_result += (
+                "@_hidden_return",
+                [match.group("return_type"), match.group("return_desc")],
+            )
 
-        raises_regex = re.compile("Raises.*^(?P<raise_type>[^\n:]*?)\n\\s{4}(?P<raise_desc>.*?)$", re.DOTALL | re.MULTILINE)
+        raises_regex = re.compile(
+            "Raises.*^(?P<raise_type>[^\n:]*?)\n\\s{4}(?P<raise_desc>.*?)$",
+            re.DOTALL | re.MULTILINE,
+        )
         for match in raises_regex.finditer(uncommented_text):
-            parse_result += ('@_hidden_raises', [match.group('raise_type'), match.group('raise_desc')])
+            parse_result += (
+                "@_hidden_raises",
+                [match.group("raise_type"), match.group("raise_desc")],
+            )
 
         return parse_result
